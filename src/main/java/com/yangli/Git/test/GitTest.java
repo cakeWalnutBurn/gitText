9c7a7f1cca9a7358c3fbec2a83ffe7740db3c3ed
yangli <415007574@qq.com>
2022-06-15 14:39:01 +0800

    第一次提交

---
+++ src/main/java/com/yangli/Git/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.yangli.Git.test;
+
+/**
+ * @author 蛋糕核桃烧
+ * @Description
+ * @create 2022-06-15 14:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Idea集成Git测试");
+    }
+}
